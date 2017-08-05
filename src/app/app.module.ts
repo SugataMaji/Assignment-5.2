a9cb996f96b7a6e8c400f940414ede120294b97e
SugataMaji <sugatamaji94@gmail.com>
2017-08-06 00:25:39 +0530

    Component

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RootComponentComponent } from './root-component/root-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RootComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
